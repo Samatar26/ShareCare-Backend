2ecd9ebb81cb5277e9f6788e779d759cf3f31895
Samatar Axmed <Samatar_267@hotmail.com>
2017-06-17 16:06:48 +0100

    Created test view

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const hapi = require('hapi');
 const server = new hapi.Server();
-const routes = require('./routes');
+const routes = require('./routes/index');
 
 server.connection({
   port: process.env.PORT || 4000,
